ad62513b2c3527e9b624ee9fdc9645d557ad8863
Esko Niiranen <eskoniiranen@codento.com>
2019-04-04 12:23:55 +0300

    Remove redundant reducer definition

--- src/state.js
+++ src/state.js
@@ -8,17 +8,3 @@ export const StateProvider = ({ reducer, initialState, children }) =>
   </StateContext.Provider>
 
 export const useStateValue = () => useContext(StateContext)
-
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case 'increment': {
-      return { ...state, count: state.count + 1 }
-    }
-    case 'decrement': {
-      return { ...state, count: state.count - 1 }
-    }
-    default: {
-      return state
-    }
-  }
-}
